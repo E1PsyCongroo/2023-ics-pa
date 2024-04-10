105d9efabe30558fec6a59bdb9557382311a36a5
focused_xy <elpsycongroo242@gmail.com>
2024-04-10 19:56:09 +0800

    init_ftrace: parsing elf file fin

---
+++ nemu/src/monitor/sdb/ftrace.c
@@ -0,0 +1,67 @@
+#include <elf.h>
+#include <stdlib.h>
+#include <debug.h>
+#include "sdb.h"
+typedef struct {
+  char *fname;
+  word_t fsize;
+  word_t faddr;
+} FtraceTable;
+
+static FtraceTable *ftrace_table = NULL;
+static char *elf_str = NULL;
+
+#define N MUXDEF(CONFIG_ISA64, 64, 32)
+
+void init_ftrace(const char* elf_file) {
+  if (elf_file == NULL) { return; }
+  FILE *fp = fopen(elf_file, "rb");
+  Assert(fp, "Can not open '%s'", elf_file);
+  /* read ELF header */
+  concat3(Elf,N,_Ehdr) elf_ehdr;
+  Assert(fread(&elf_ehdr, sizeof elf_ehdr, 1, fp) != sizeof elf_ehdr, "%s is not a elf file\n", elf_file);
+  Assert(!strncmp((char*)elf_ehdr.e_ident, "\x7f""ELF", 4), "%s is not a elf file\n", elf_file);
+  /* read Section header */
+  fseek(fp, elf_ehdr.e_shoff, SEEK_SET);
+  concat3(Elf,N,_Shdr) elf_shdr[elf_ehdr.e_shnum];
+  fread(&elf_shdr, elf_ehdr.e_shentsize, elf_ehdr.e_shnum, fp);
+  concat3(Elf,N,_Off) sym_offset = 0, str_offset = 0;
+  concat3(Elf,N,_Word) sym_size = 0, str_size = 0;
+  for (size_t i = 0; i < elf_ehdr.e_shnum; i++) {
+    if (elf_shdr[i].sh_type == SHT_SYMTAB && !sym_offset) {
+      sym_offset = elf_shdr[i].sh_offset;
+      sym_size = elf_shdr[i].sh_size;
+    }
+    else if (elf_shdr[i].sh_type == SHT_STRTAB && !str_offset) {
+      str_offset = elf_shdr[i].sh_offset;
+      str_size = elf_shdr[i].sh_size;
+    }
+  }
+  /* read Symbol table */
+  size_t elf_symentnum = sym_size / sizeof(concat3(Elf,N,_Sym));
+  concat3(Elf,N,_Sym) elf_sym[elf_symentnum];
+  fseek(fp, sym_offset, SEEK_SET);
+  fread(elf_sym, sym_size, 1, fp);
+  /* read String table */
+  elf_str = malloc((sizeof *elf_str) * str_size);
+  fseek(fp, str_offset, SEEK_SET);
+  fread(elf_str, str_size, 1, fp);
+  /* generating ftrace table*/
+  size_t ftrace_table_num = 0;
+  for (size_t i = 0; i < elf_symentnum; i++) {
+    if (concat3(ELF,N,_ST_TYPE)(elf_sym[i].st_info) == STT_FUNC) {
+      ftrace_table_num++;
+    }
+  }
+  if (!ftrace_table_num) { return; }
+  ftrace_table = malloc((sizeof *ftrace_table) * ftrace_table_num);
+  FtraceTable *tp = ftrace_table;
+  for (size_t i = 0; i < elf_symentnum; i++) {
+    if (concat3(ELF,N,_ST_TYPE)(elf_sym[i].st_info) == STT_FUNC) {
+      tp->faddr = elf_sym[i].st_value;
+      tp->fsize = elf_sym[i].st_size;
+      tp->fname = &elf_str[elf_sym[i].st_name];
+      tp++;
+    }
+  }
+}
