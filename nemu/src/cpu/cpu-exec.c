344ee7ae6b192f6ca0a4db5b85fd7c47abccc896
focused_xy <elpsycongroo242@gmail.com>
2024-04-12 08:46:06 +0800

    am nemu.mk NEMUFLAGS override, fix iringbuf print condition, fix memmove bug

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -144,7 +144,7 @@ void cpu_exec(uint64_t n) {
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
             ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           nemu_state.halt_pc);
-      if (nemu_state.halt_ret != 0) {
+      if (nemu_state.state == NEMU_ABORT || nemu_state.halt_ret != 0) {
         IFDEF(CONFIG_IRINGBUF, print_i_ring_buf());
       }
       // fall through
