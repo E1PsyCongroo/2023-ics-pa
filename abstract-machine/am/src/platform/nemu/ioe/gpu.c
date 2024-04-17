e0debb8cf268b5b835450a6ba8af4ad041311832
focused_xy <elpsycongroo242@gmail.com>
2024-04-17 11:57:12 +0800

    PA2 fin

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,17 +4,32 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  // int i;
+  // int w = io_read(AM_GPU_CONFIG).width;
+  // int h = io_read(AM_GPU_CONFIG).height;
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < w * h; i++) fb[i] = 0;
+  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = (inl(VGACTL_ADDR) >> 16) & 0xffff, .height = inl(VGACTL_ADDR) & 0xffff,
     .vmemsz = 0
   };
+  cfg->vmemsz = cfg->width * cfg->height * sizeof(uint32_t);
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t width = io_read(AM_GPU_CONFIG).width;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *pixels = ctl->pixels;
+  for (int y = ctl->y; y < ctl->y + ctl->h; y++) {
+    for (int x = ctl->x; x < ctl->x + ctl->w; x++) {
+      fb[x + y * width] = pixels[(y - ctl->y) * ctl->w + (x - ctl->x)];
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
