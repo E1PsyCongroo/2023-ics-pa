4700ae9bd2fa2e762398f4a2d6726d400f8806cc
focused_xy <elpsycongroo242@gmail.com>
2024-04-24 00:07:58 +0800

    SDL_FillRect fin

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -10,14 +10,22 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
+  uint32_t *pixels_ptr = dst->pixels;
+  int base = (dstrect->y * dst->w) + dstrect->x;
+  for (int i = 0; i < dstrect->h; i++) {
+    for (int j = 0; j < dstrect->w; j++) {
+      pixels_ptr[base + j] = color;
+    }
+    base += dst->w;
+  }
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
   if (x == 0 && y == 0 && w == 0 && h == 0) {
-    NDL_DrawRect(s->pixels, 0, 0, s->w, s->h);
+    NDL_DrawRect((void*)s->pixels, 0, 0, s->w, s->h);
   }
   else {
-    NDL_DrawRect(s->pixels, x, y, w, h);
+    NDL_DrawRect((void*)(s->pixels+s->format->BytesPerPixel*(x+(y*s->w))), x, y, w, h);
   }
 }
 
